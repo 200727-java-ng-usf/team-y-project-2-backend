6d645d5214574ddd2dfdc7f816ecf620fb7e42b7
Stephen Brasel <wearyroad@gmail.com>
2020-09-23 15:08:38 -0400

    modified database sql file - removed cuisine, amg_meal_prior_restaurants, etc.

--- A-MEAL-GOS/src/main/resources/amealgo.sql
+++ A-MEAL-GOS/src/main/resources/amealgo.sql
@@ -22,8 +22,8 @@ create table amg_users(
 create table amg_meal( 
 	amg_meal_id						serial, 
 	num_votes 						int default 3, -- determines how many votes each user gets during the voting period 
-	invite_code						varchar(255) unique not null, 
 	meal_name						varchar(255) not null, 
+	meal_location					text,
 	constraint amg_meal_id_pk 
 		primary key(amg_meal_id) 
 ); 
@@ -31,38 +31,42 @@ create table amg_meal(
 -- used to get previously enjoyed restaurants. 
 create table amg_likes( 
 	amg_like_id						serial, 
-	restaurant						varchar(255), 
+	restaurant						text, 
 	amg_user_id 					int, 
 	constraint amg_like_id_pk 
 		primary key(amg_like_id) 
 ); 
 
 -- May not be used 
-create table amg_cuisines( 
-	amg_cuisine_id					serial, 
-	cuisine 						varchar(255), 
-	constraint amg_cuisine_id_pk 
-		primary key(amg_cuisine_id) 
-); 
+--create table amg_cuisines( 
+--	amg_cuisine_id					serial, 
+--	cuisine 						varchar(255), 
+--	constraint amg_cuisine_id_pk 
+--		primary key(amg_cuisine_id) 
+--); 
 
 -- used in the meal to vote on which restaurant to go to 
 create table amg_votes( 
 	amg_vote_id						serial, 
-	restaurant						varchar(255), 
+	restaurant						text, 
 	vote_meal_id					int, 
+	amg_voter_id 					int, 
 	constraint amg_vote_id_pk 
 		primary key(amg_vote_id), 
 	constraint vote_meal_fk 
 		foreign key (vote_meal_id) 
-		references amg_meal (amg_meal_id) 
+		references amg_meal (amg_meal_id), 
+	constraint user_vote_fk 
+		foreign key (amg_voter_id) 
+		references amg_users (amg_user_id) 
 ); 
 
 -- all the restaurants being chosen from any given meal 
 create table amg_meal_restaurants( 
 	amg_meal_restaurant_id			serial, 
 	amg_meal_id 					int, 
-	amg_restaurant 					varchar(255), 
-	constraint amg_meal_restaurant_id_pk  
+	amg_restaurant 					text, 
+	constraint amg_meal_restaurant_id_pk 
 		primary key(amg_meal_restaurant_id), 
 	constraint meal_restaurants_fk 
 		foreign key (amg_meal_id) 
@@ -71,16 +75,16 @@ create table amg_meal_restaurants(
 
 -- all the restaurants that were the final choice from any given meal, used for history 
 -- may not be used 
-create table amg_meal_prior_restaurants( 
-	amg_meal_prior_restaurant_id	serial, 
-	amg_meal_id 					int, 
-	amg_restaurant 					varchar(255), 
-	constraint amg_meal_prior_restaurant_id_pk 
-		primary key(amg_meal_prior_restaurant_id), 
-	constraint meal_prior_restaurants_fk 
-		foreign key (amg_meal_id) 
-		references amg_meal (amg_meal_id) 
-);
+--create table amg_meal_prior_restaurants( 
+--	amg_meal_prior_restaurant_id	serial, 
+--	amg_meal_id 					int, 
+--	amg_restaurant 					text, 
+--	constraint amg_meal_prior_restaurant_id_pk 
+--		primary key(amg_meal_prior_restaurant_id), 
+--	constraint meal_prior_restaurants_fk 
+--		foreign key (amg_meal_id) 
+--		references amg_meal (amg_meal_id) 
+--);
 
 -- Junction table of users and meals: shows which meals have which users.
 create table amg_meal_users( 
