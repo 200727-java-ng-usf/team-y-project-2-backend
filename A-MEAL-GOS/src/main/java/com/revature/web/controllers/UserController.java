dc585dd3a5f7e7c406cf1a06e07335ad66845a0e
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-09-24 19:30:19 -0400

    Added web package -> aspects, controllers, filters packages; starting on /users controller

---
+++ A-MEAL-GOS/src/main/java/com/revature/web/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.revature.web.controllers;
+
+import com.revature.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.userService = service;
+    }
+
+    @PostMapping
+    public void newUser()
+
+
+}
