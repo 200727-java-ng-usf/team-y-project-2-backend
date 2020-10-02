4d12c8efc44ecd6a1b08d63bb74f27a0d8a6c245
capolimia <capolimia@gmail.com>
2020-10-02 06:57:59 -0400

    tsakfdas

--- A-MEAL-GOS/src/main/java/com/revature/web/filters/CorsFilter.java
+++ A-MEAL-GOS/src/main/java/com/revature/web/filters/CorsFilter.java
@@ -16,7 +16,7 @@ public class CorsFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", ":http://localhost4200");
         httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type");
         httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
         filterChain.doFilter(httpServletRequest, httpServletResponse);
