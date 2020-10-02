e4c13e4ae5abc9f803c8138264cabe71d3471a67
Daniela Yacobucci Lapaitis <dyacobucci47@gmail.com>
2020-10-02 06:21:45 -0400

    git rid of amg_meal_users_voted on import.sql

--- A-MEAL-GOS/src/main/resources/import.sql
+++ A-MEAL-GOS/src/main/resources/import.sql
@@ -112,6 +112,3 @@ insert into amg_meal_users values (1, 23);
 insert into amg_meal_users values (1, 24);
 insert into amg_meal_users values (1, 25);
 insert into amg_meal_users values (1, 26);
-
-insert into amg_meal_users_voted values (1, 1);
-insert into amg_meal_users_voted values (1, 2);
